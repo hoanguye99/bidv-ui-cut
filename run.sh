65ade1752efea72a0457b54b5f7904fbd7f6dfab
Kamiya <hawk01525@gmail.com>
2022-10-04 20:31:46 +0700

    newspage + news-detail

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 sass  scss/global.scss dist/css/all.css
 sass  scss/components/homepage.scss dist/css/homepage.css
 sass  scss/components/hdsd.scss dist/css/hdsd.css
+sass  scss/components/news.scss dist/css/news.css
