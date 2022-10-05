a56fd66f931ebe2282c9426cf130a14dee115a47
Kamiya <hawk01525@gmail.com>
2022-10-05 18:21:46 +0700

    add card product + edit news detail scss

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ sass  scss/global.scss dist/css/all.css
 sass  scss/components/homepage.scss dist/css/homepage.css
 sass  scss/components/hdsd.scss dist/css/hdsd.css
 sass  scss/components/news.scss dist/css/news.css
+sass  scss/components/news-detail.scss dist/css/news-detail.css
+sass  scss/components/product.scss dist/css/product.css
